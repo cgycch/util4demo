932398c7ecaee127c6fb554110abafb439365f20
cch <1136856486@qq.com>
2018-09-29 08:15:36 +0800

    lambada

--- src/main/java/com/cch/common/utils/App.java
+++ src/main/java/com/cch/common/utils/App.java
@@ -1,5 +1,11 @@
 package com.cch.common.utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.cch.common.utils.excel.annotation.ExcelField;
 import com.cch.common.utils.excel.demo.ExcelUtil;
 
 /**
@@ -15,5 +21,17 @@ public class App
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// Field sorting
+    	List<Object[]> annotationList = new ArrayList<Object[]>();
+		Collections.sort(annotationList, new Comparator<Object[]>() {
+			@Override
+			public int compare(Object[] o1, Object[] o2) {
+				return new Integer(((ExcelField)o1[0]).sort()).compareTo(
+						new Integer(((ExcelField)o2[0]).sort()));
+			};
+		});
+		annotationList.sort((Object[] o1, Object[] o2) -> {
+			return ((ExcelField) o1[0]).sort()-((ExcelField) o2[0]).sort();
+		});
     }
 }
